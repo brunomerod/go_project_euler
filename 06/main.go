605aea89ba71fd76bcef47e7331788887dfe252c
brunomerod <bruno.sushirod@gmail.com>
2020-04-15 22:18:27 -0300

    Til 20

--- 06/main.go
+++ 06/main.go
@@ -5,5 +5,27 @@ import (
 )
 
 func main() {
+	var fn int
+	fmt.Printf("First numbers: ")
+	_, err := fmt.Scanf("%d", &fn)
+
+	var sqr int
+	var sum int
+
+	for i := 1; i <= fn; i++ {
+		sqr = sqr + (i * i)
+	}
+
+	for i := 1; i <= fn; i++ {
+		sum = sum + i
+	}
+
+	sum = sum * sum
+
+	if err != nil || fn < 0 {
+		fmt.Println("Not possible")
+	} else {
+		fmt.Printf("\n%d - %d = %d\n", sum, sqr, sum-sqr)
+	}
 
 }
