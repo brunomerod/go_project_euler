d46f15a697a0b7d4973938b2393e5fa97ecf9cda
brunomerod <bruno.sushirod@gmail.com>
2020-04-15 22:51:05 -0300

    07 done

--- 07/main.go
+++ 07/main.go
@@ -4,5 +4,47 @@ import (
 	"fmt"
 )
 
+func isPrime(n int) bool {
+	var div []int
+
+	for i := 1; i <= n; i++ {
+		if n%i == 0 {
+			div = append(div, i)
+		}
+		//fmt.Println(".")
+		if len(div) > 2 {
+			break
+		}
+	}
+
+	//fmt.Println(div)
+	if len(div) == 2 {
+		return true
+	}
+
+	return false
+}
+
 func main() {
+	var np int
+	fmt.Printf("The nth prime: ")
+	_, err := fmt.Scanf("%d", &np)
+	//For project Euler 1 is not a prime :(
+	for n, i := 2, 1; i <= np; n++ {
+		if isPrime(n) {
+			i++
+
+			//fmt.Printf("%d, ", n)
+
+			//Print only the last one
+			if i > np {
+				fmt.Println("\nNth number", n)
+			}
+		}
+	}
+
+	//fmt.Println(n)
+	if err != nil {
+		fmt.Println(err)
+	}
 }
